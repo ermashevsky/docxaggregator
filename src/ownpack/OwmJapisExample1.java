c52b3725858c4b3ede2b15aa58af3c6c51bd8c49
Denic <ermashevsky@gmail.com>
2015-12-03 16:12:11 +0300

    OpenWeatherMap API интерфейс погоды

--- src/ownpack/OwmJapisExample1.java
+++ src/ownpack/OwmJapisExample1.java
@@ -3,66 +3,43 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxapplicationdemo;
+package ownpack;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import net.aksingh.owmjapis.CurrentWeather;
 import net.aksingh.owmjapis.OpenWeatherMap;
 import org.json.JSONException;
+
 
 /**
  *
  * @author denic
  */
-public class FXMLDocumentController implements Initializable {
+public class OwmJapisExample1 {
     
-    @FXML
-    private Label city;
-    
-    @FXML
-    private Label date;
-    
-    @FXML
-    private Label current_temp;
-    
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) throws IOException, MalformedURLException,JSONException {
-        System.out.println("You clicked me!");
-        
+    public static void main(String[] args)
+            throws IOException, MalformedURLException,JSONException{
+
+        // declaring object of "OpenWeatherMap" class
         OpenWeatherMap owm = new OpenWeatherMap("");
         owm.setApiKey("95b844c054292e842f1cb4fc3d5b4367");
         owm.setUnits(OpenWeatherMap.Units.METRIC);
-        owm.setLang(OpenWeatherMap.Language.RUSSIAN);
-        
+
         // getting current weather data for the "London" city
-        CurrentWeather cwd = owm.currentWeatherByCityName("Sochi");
-        
+        CurrentWeather cwd = owm.currentWeatherByCityName("Saratov");
+
         //printing city name from the retrieved data
         System.out.println("City: " + cwd.getCityName());
-        city.setText(cwd.getCityName());
         
+
         // printing the max./min. temperature
-        System.out.println("Temperature : " + cwd.getMainInstance().getMaxTemperature()
+        System.out.println("Temperature: " + cwd.getMainInstance().getMaxTemperature()
                             + "/" + cwd.getMainInstance().getMinTemperature() + "\'C");
-        
-        current_temp.setText(cwd.getMainInstance().getMaxTemperature() + "/" + cwd.getMainInstance().getMinTemperature() + " C");
         // printing the max./min. temperature
         System.out.println("Pressure: " + cwd.getMainInstance().getPressure() + " hpa");
         System.out.println("Humidity: " + cwd.getMainInstance().getHumidity() + " %");
-        
+                            
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
     
 }
